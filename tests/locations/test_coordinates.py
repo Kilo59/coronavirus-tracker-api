7bb4f2d981fde5fc7ba1cc2c305a310b6d257175
Gabriel <gabriel59kg@gmail.com>
2020-05-10 13:20:51 -0400

    update coordinate tests

--- tests/locations/test_coordinates.py
+++ tests/locations/test_coordinates.py
@@ -2,14 +2,14 @@ from unittest import mock
 
 import pytest
 
-from app import coordinates
+from app import location
 
 
 @pytest.mark.parametrize("latitude, longitude", [("1", "2"), (100, "2"), (-3, 0), (-10, -10000000)])
 def test_coordinates_class(latitude, longitude):
-    coord_obj = coordinates.Coordinates(latitude=latitude, longitude=longitude)
+    coord_obj = location.Coordinates(latitude=latitude, longitude=longitude)
 
     # validate serialize
-    check_obj = {"latitude": latitude, "longitude": longitude}
+    check_obj = {"latitude": float(latitude), "longitude": float(longitude)}
 
-    assert coord_obj.serialize() == check_obj
+    assert coord_obj.dict() == check_obj
